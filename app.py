afe92790fcb3628a311e0fed48c8416f8ad933a6
Pontus Liedgren <pop2strong4u@gmail.com>
2020-02-03 23:17:46 +0100

    API test script

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image
 from io import BytesIO
 import requests
 
-response = requests.get(f'http://api.tronalddump.io/random/meme')
+response = requests.get('http://api.tronalddump.io/random/meme')
 img = Image.open(BytesIO(response.content))
 img.show()
 
